12b1902a4597fb705534380f388bd3533d44742d
Kailinlow <tranhuutoan511@gmail.com>
2024-09-22 02:10:14 +0700

    add Product dto

---
+++ product-service/src/main/java/com/kltn/product_service/component/product/dto/response/ProductResponse.java
@@ -0,0 +1,31 @@
+package com.kltn.product_service.component.product.dto.response;
+
+
+import org.springframework.lang.Nullable;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record ProductResponse(
+        String id,
+        Instant createdAt,
+        Instant updatedAt,
+        @Nullable
+        String createdBy,
+        @Nullable
+        String updatedBy,
+        String name,
+        String imageUrl,
+        String description,
+        BigDecimal cost,
+        BigDecimal price,
+        BigDecimal marketPrice,
+        Long stockQuantity,
+        BrandResponse brand
+) {
+    public record BrandResponse(
+        String id,
+        String name,
+        String information
+    ){}
+}
