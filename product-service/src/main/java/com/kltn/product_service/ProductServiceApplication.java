466ce56feea9fd7acef543303ef80b3c56c68bb5
Kailinlow <tranhuutoan511@gmail.com>
2024-12-02 03:12:00 +0700

    add Docker

--- product-service/src/main/java/com/kltn/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/kltn/product_service/ProductServiceApplication.java
@@ -2,7 +2,6 @@ package com.kltn.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
